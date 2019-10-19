fd165c4c6205c0c78f5fae5be0156b3685d3b27d
Bayu Pratama <bayzpratama7@gmail.com>
2019-10-19 13:41:22 +0700

    Add file Python

---
+++ Operator.py
@@ -0,0 +1,15 @@
+# Operator merupakan simbol-simbol yang digunakan untuk melakukan operasi tertentu
+# Ada 6 operator yang wajib diketahui, yaitu: Operator Aritmatika, Pembanding/Relasi-
+# -Penugasan, Logika, Bitwise, Ternary.
+
+# 1. Operator Aritmatika
+#   Contoh operator aritmatika: penjumlahan (+) , pengurangan (-), pembagian (/),
+#   Perkalian (*), sisa bagi (%), pemangkatan (**)
+
+# Contoh program penghitungan sederhana
+
+a = input("Input nilai a: ")
+b = input("Input nilai b: ")
+
+c = a + b
+# print("Hasil %d + %d = %d" % a,b,c)
---
+++ Statement-Variable-Data.py
@@ -0,0 +1,46 @@
+# Penulisan pada bahasa Python :)
+
+nama = "Bayu Pratama"
+print("Hello World! Kita mulai belajar python dari nol :)")
+
+# Di bahasa Python, kita tidak perlu memberi tanda titik koma ( ; ) pada akhir kalimat.
+# Tetapi jika kita ingin menulis lebih dari satu statement dalam satu baris, maka harus menggunakan titik koma.
+
+print("Hello World"); print("Tutorial Python bagi pemula")
+
+# <----------------------------------------------------------------------------------------------------------->
+
+# Variable
+# penulisan variable di Python adalah nama_variable = <nilai>
+
+variable = "Ini variable"
+variable2 = 5
+
+print(variable); print(variable2)
+
+# Kelebihan di Python adalah, kita tidak harus mendeklarasikan tipe datanya
+# Python akan secara otomatis mengenali jenis data atau tipe data yang tersimpan dalam sebuah variabel.
+
+# <----------------------------------------------------------------------------------------------------------->
+
+# 1. Jenis Tipe data angka adalah int (integer) dan float
+#   Contoh int: 1, 2, 3, 4, 5
+#   Contoh float: 0.5, 1.5, 2.5, 3.5
+
+harga = 50000 #tipe data int
+berat_badan = 47.65 #Tipe data float
+
+
+# 2. Jenis tipe data teks adalah Char dan String
+#   Contoh Char: 'R' 'A'
+#   Contoh String (kumpulan karakter): "Aku sedang belajar programming"
+
+nama = "Bayu Pratama"
+jenis_kelamin = 'L'
+umur = "20 tahun"
+
+# 3. Jenis tipe data boolean
+#   Tipe data boolean adalah tipe data yang hanya memiliki dua nilai yaitu True dan False atau 0 dan 1
+
+diam = True
+menyala = 1
